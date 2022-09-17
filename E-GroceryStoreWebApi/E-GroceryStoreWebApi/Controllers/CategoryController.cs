32d324f1f1f7e3496a1cbe45b541868bebd8ccbe
unknown <mindc1july68@iiht.tech>
2022-09-17 08:04:38 +0530

    E-Grocery Repository

---
+++ E-GroceryStoreWebApi/E-GroceryStoreWebApi/Controllers/CategoryController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using E_GroceryStoreWebApi.DataAccess;
+using E_GroceryStoreWebApi.Models;
+
+namespace E_GroceryStoreWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly GroceryStoreDbContext _context;
+
+        public CategoryController(GroceryStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Category
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryModel>>> GetcategoryModel()
+        {
+            return await _context.categoryModel.ToListAsync();
+        }
+
+        // GET: api/Category/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CategoryModel>> GetCategoryModel(int id)
+        {
+            var categoryModel = await _context.categoryModel.FindAsync(id);
+
+            if (categoryModel == null)
+            {
+                return NotFound();
+            }
+
+            return categoryModel;
+        }
+
+        // PUT: api/Category/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategoryModel(int id, CategoryModel categoryModel)
+        {
+            if (id != categoryModel.CategoryId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(categoryModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoryModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Category
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<CategoryModel>> PostCategoryModel(CategoryModel categoryModel)
+        {
+            _context.categoryModel.Add(categoryModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCategoryModel", new { id = categoryModel.CategoryId }, categoryModel);
+        }
+
+        // DELETE: api/Category/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<CategoryModel>> DeleteCategoryModel(int id)
+        {
+            var categoryModel = await _context.categoryModel.FindAsync(id);
+            if (categoryModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.categoryModel.Remove(categoryModel);
+            await _context.SaveChangesAsync();
+
+            return categoryModel;
+        }
+
+        private bool CategoryModelExists(int id)
+        {
+            return _context.categoryModel.Any(e => e.CategoryId == id);
+        }
+    }
+}
